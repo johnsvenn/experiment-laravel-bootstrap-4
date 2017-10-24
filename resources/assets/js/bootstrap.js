09f923bb034dcaf0563a644daaf4c050b5911981
John Elliott <johnsvenn@googlemail.com>
2017-10-24 11:18:46 +0100

    Setup the .js files

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -8,9 +8,21 @@ window._ = require('lodash');
  */
 
 try {
+	
     window.$ = window.jQuery = require('jquery');
 
-    require('bootstrap-sass');
+    require('bootstrap/js/dist/alert');
+    require('bootstrap/js/dist/button');
+    require('bootstrap/js/dist/carousel');
+    require('bootstrap/js/dist/dropdown');
+    require('bootstrap/js/dist/index');
+    require('bootstrap/js/dist/modal');
+    require('bootstrap/js/dist/popover');
+    require('bootstrap/js/dist/scrollspy');
+    require('bootstrap/js/dist/tab');
+    require('bootstrap/js/dist/tooltip');
+    require('bootstrap/js/dist/util'); 
+    
 } catch (e) {}
 
 /**
